fba7ea786af05d56b1d5f489ac8d7a3b75c53b10
I Made Purnama Yasa <62276883+imadepurnamayasa@users.noreply.github.com>
2024-03-15 05:40:14 +0800

    merapikan

--- database.php
+++ database.php
@@ -6,10 +6,10 @@ $username = "root";
 $password = "root";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	// set the PDO error mode to exception
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	//echo "Connected successfully";
+} catch (PDOException $e) {
+	echo "Connection failed: " . $e->getMessage();
 }

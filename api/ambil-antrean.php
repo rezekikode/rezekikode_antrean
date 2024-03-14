7a6213c95e2407424bf6e1206304924101247d47
I Made Purnama Yasa <62276883+imadepurnamayasa@users.noreply.github.com>
2024-03-14 20:13:32 +0800

    query nomor

--- api/ambil-antrean.php
+++ api/ambil-antrean.php
@@ -6,5 +6,18 @@ $id_jenis_loket = 0;
 $metode = $_SERVER['REQUEST_METHOD'];
 
 if ($metode !== 'post') {
-    die('Metode tidak sesuai');
+    die('Metode tidak sesuai.');
 }
+
+$query =
+    "
+    SELECT 
+    MAX(nomor) AS nomor
+    FROM 
+    WHERE 
+    id_lokasi = :id_lokasi 
+    AND id_loket = :id_loket 
+    AND id_jenis_lokasi = :id_jenis_loket
+    ";
+
+$nomor_sekarang = $nomor + 1;

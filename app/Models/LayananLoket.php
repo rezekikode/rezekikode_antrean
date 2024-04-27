bb1b22e571e6ac968bd84ae01c73c9e1fc7f04a2
I Made Purnama Yasa <62276883+imadepurnamayasa@users.noreply.github.com>
2024-04-27 09:43:26 +0800

    change: 2024-04-27 Layanan Loket

--- app/Models/LayananLoket.php
+++ app/Models/LayananLoket.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class LoketLayanan extends Model
+class LayananLoket extends Model
 {
     use HasFactory;
 }

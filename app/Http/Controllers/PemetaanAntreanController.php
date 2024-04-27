88ae8ce4cb8f1e9df6c53b9b7eebf52df280a8d5
I Made Purnama Yasa <62276883+imadepurnamayasa@users.noreply.github.com>
2024-04-27 12:03:38 +0800

    ADD: 2024-04-27 Pemetaan Antrean

---
+++ app/Http/Controllers/PemetaanAntreanController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PemetaanAntreanController extends Controller
+{
+    // Tambahkan method-method controller di sini
+
+    public function index()
+    {
+        // Kode untuk menampilkan halaman index
+    }
+
+    public function create()
+    {
+        // Kode untuk menampilkan halaman create
+    }
+
+    public function store(Request $request)
+    {
+        // Kode untuk menyimpan data dari form screate
+    }
+
+    public function show($id)
+    {
+        // Kode untuk menampilkan data berdasarkan ID
+    }
+
+    public function edit($id)
+    {
+        // Kode untuk menampilkan halaman edit
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Kode untuk mengupdate data dari form edit
+    }
+
+    public function destroy($id)
+    {
+        // Kode untuk menghapus data berdasarkan ID
+    }
+}

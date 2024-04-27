7f9423cc72302d64ceadbb994563492e4974015d
I Made Purnama Yasa <62276883+imadepurnamayasa@users.noreply.github.com>
2024-04-27 09:48:27 +0800

    CHANGE: 2024-04-27 Tampil Antrean

--- resources/views/tampil/index.blade.php
+++ resources/views/tampil/index.blade.php
@@ -38,6 +38,7 @@
                                 <h6 class="card-text text-center"><?= $antrean_menunggu->tanggal_ambil ?></h6>
                             <?php else : ?>
                                 <h5 class="card-text text-center">0</h5>
+                                <h6 class="card-text text-center">-</h6>
                             <?php endif ?>
                         </div>
                     </div>

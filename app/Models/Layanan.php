d57156a468fc43337cf98a3c586add86591791b6
I Made Purnama Yasa <62276883+imadepurnamayasa@users.noreply.github.com>
2024-04-27 10:17:49 +0800

    CHANGE: 2024-04-27 Layanan Model

--- app/Models/Layanan.php
+++ app/Models/Layanan.php
@@ -9,14 +9,5 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Layanan extends Model
 {
     use HasFactory;
-
-    public function lokasi()
-    {
-        return $this->belongsTo(Lokasi::class);
-    }
-
-    public function lokets(): BelongsToMany
-    {
-        return $this->belongsToMany(Loket::class)->withTimestamps();
-    }
+    
 }

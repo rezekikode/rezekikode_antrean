0715e82570bdd6f72272f094197e9924e2e2aacb
I Made Purnama Yasa <62276883+imadepurnamayasa@users.noreply.github.com>
2024-04-27 10:32:18 +0800

    CHANGE: 2024-04-27 View Panggil Antrean

--- resources/views/panggil/index.blade.php
+++ resources/views/panggil/index.blade.php
@@ -1,10 +1,8 @@
-Loket
+Layanan
 <select name="" id="" onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
-    <option value="<?= route('panggil') ?>">-PILIH LOKET-</option>
-    <?php foreach ($lokets as $loket) : ?>
-        <?php foreach ($loket->layanans as $layanan) : ?>
-            <option value="<?= route('panggil', ['layanan_id' => $layanan->id, 'loket_id' => $loket->id]) ?>" <?= $layanan->id === $layanan_id && $loket->id === $loket_id ? 'selected' : '' ?>><?= $layanan->layanan . ' - ' . $loket->loket ?></option>
-        <?php endforeach ?>
+    <option value="<?= route('panggil') ?>">-PILIH LAYANAN-</option>
+    <?php foreach ($layanans as $layanan) : ?>
+        <option value="<?= route('panggil', ['lokasi_id' => $lokasi_id, 'layanan_id' => $layanan->id, 'loket_id' => $loket_id]) ?>" <?= $layanan->id === $layanan_id ? 'selected' : '' ?>><?= $layanan->layanan ?></option>
     <?php endforeach ?>
 </select>
 <span id="clock"></span> | <span id="countdown"></span>
